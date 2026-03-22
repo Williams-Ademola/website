ce732e930a97c698466557ac8eff397e9261451c
Williams Ademola <69696873+Williams-Ademola@users.noreply.github.com>
2026-03-22 16:59:51 -0500

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,24 +6,23 @@ export function Header() {
     <header className="mb-16 space-y-4">
       <h1 className="text-4xl font-bold mb-4 animate-fade-in text-white">
         <span className="inline-block">
-          <ScrambleText text="shoubhit dash" />
+          <ScrambleText text="williams ogunjide" />
         </span>
       </h1>
       <div className="flex flex-col gap-2 text-gray-400">
         <div className="flex items-center gap-2">
           <MapPin className="w-4 h-4" />
-          chennai, india
+          winnipeg, canada.
         </div>
         <div className="flex items-center gap-2">
           <Building2 className="w-4 h-4" />
-          co-founder and cto @ mocha
+          Crest
         </div>
       </div>
       <p className="leading-relaxed animate-fade-in-up">
-        i&apos;m a 20 y/o cs undergrad student. i love building things and
-        solving problems. i enjoy language design, theoretical computer science
-        and i live on the terminal. if i&apos;m not coding, i&apos;m probably
-        doing cardistry, watching movies or obsessing over mechanical keyboards.
+        Hi there, I'm Williams. I'm a CS and Math undergrad student with a near crippling tac fps addiction. 
+        I like consuming lovecraftian literature and building things. 
+        I enjoy stochastic calculus, machine learning and making my problems go away with the terminal. 
       </p>
     </header>
   )
